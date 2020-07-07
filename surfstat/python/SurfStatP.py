ca102634b5bfc2196dbf06cc0994da5c302e1ba1
Şeyma Bayrak <sheymaba@gmail.com>
2020-07-07 16:29:29 +0200

    implementing the case, where slm has a 'dfs' key

--- surfstat/python/SurfStatP.py
+++ surfstat/python/SurfStatP.py
@@ -24,19 +24,17 @@ def var2mat(var):
 def py_SurfStatP(slm, mask=None, clusthresh=0.001):
     
     l, v =np.shape(slm['t'])
-    
+
     if mask is None:
         mask = np.ones((1,v), dtype=bool)
-    
-    
+
     df = np.zeros((2,2))
-    
     ndf = len(slm['df'])
     df[0, 0:ndf] = slm['df']
     df[1, 0:2] = slm['df'][ndf-1]
     
-    #if 'dfs' in slm.keys():
-    #    df[0, ndf-1] = ...
+    if 'dfs' in slm.keys():
+        df[0, ndf-1] = slm['dfs'][mask > 0].mean()
     
     if v == 1:
         varA = np.concatenate((np.array([[10]]), slm['t']), axis=1)
@@ -108,7 +106,6 @@ def py_SurfStatP(slm, mask=None, clusthresh=0.001):
         peak = []
         clus = []
         clusid = []
-    
     else:
         # NEED TO BE CALLED FROM PYTHON SURFSTATPEAKCLUSTER
         peak, clus, clusid  = sw.matlab_SurfStatPeakClus(slm, mask, thresh,
@@ -135,7 +132,6 @@ def py_SurfStatP(slm, mask=None, clusthresh=0.001):
                                         nargout=2)
         pp = np.array(pp)
         clpval = np.array(clpval)
-        
         peak['P'] = pp[:,1:len(peak['t'])+1].T
         pval = {}
         pval['P'] = pp[0,len(peak['t']) + np.arange(1,v+1)]
@@ -147,7 +143,6 @@ def py_SurfStatP(slm, mask=None, clusthresh=0.001):
         
         x = np.concatenate((np.array([[0]]), clus['clusid']), axis=0)
         y = np.concatenate((np.array([[1]]), clus['P']), axis=0)
-        
         pval['C'] = interp1d(x.flatten(),y.flatten())(clusid)
     
     # NEED TO BE CALLED FROM PYTHON STAT_THRESHOLD
@@ -166,9 +161,8 @@ def py_SurfStatP(slm, mask=None, clusthresh=0.001):
                                 nargout=1)
     tlim = np.array(tlim)
     tlim = tlim[0,1]
-
     pval['P'] = pval['P'] * (slm['t'][0,:] > tlim) + (slm['t'][0,:] <= tlim)
     pval['mask'] = mask
 
     return pval, peak, clus, clusid
-    
+
