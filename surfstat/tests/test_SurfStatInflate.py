8ee0869ab69009fd68ef025f611052075083dd1c
Şeyma Bayrak <sheymaba@gmail.com>
2020-08-17 16:08:06 +0200

    add more elaborate tests for SurfStatInflate

--- surfstat/tests/test_SurfStatInflate.py
+++ surfstat/tests/test_SurfStatInflate.py
@@ -7,7 +7,7 @@ import surfstat_wrap as sw
 import numpy as np
 import pytest
 
-def dummy_test(surf, w=0.5, spherefile='sphere.obj'):
+def dummy_test(surf, w=0.5, spherefile=None):
 
     try:
         # wrap matlab functions
@@ -60,6 +60,162 @@ def test_4():
     
 def test_5():
     v = 40962
+    surf = {}
+    surf['coord'] = np.random.randint(low=-(v-1), high=(v+1), size=(3,v))
+    dummy_test(surf)
+    
+def test_6():
+    v = 81924
+    surf = {}
+    surf['coord'] = np.random.rand(3,v)
+    dummy_test(surf)
+
+def test_7():
+    v = 81924
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-3, high=3, size=(3,v))
+    dummy_test(surf)
+    
+def test_8():
+    v = 81924
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-300, high=300, size=(3,v))
+    dummy_test(surf)
+    
+def test_9():
+    v = 81924
+    surf = {}
+    surf['coord'] = np.random.randint(low=-1000, high=1000, size=(3,v))
+    dummy_test(surf)
+    
+def test_10():
+    v = 81924
+    surf = {}
+    surf['coord'] = np.random.randint(low=-(v-1), high=(v+1), size=(3,v))
+    dummy_test(surf)   
+
+def test_11():
+    v = 40962
+    surf = {}
+    surf['coord'] = np.random.rand(3,v)
+    surf['coord'][0,:] = np.ones((1,v)) * -1
+    dummy_test(surf)
+
+def test_12():
+    v = 40962
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-3, high=3, size=(3,v))
+    surf['coord'][0,:] = np.ones((1,v)) * -1
+    dummy_test(surf)
+    
+def test_13():
+    v = 40962
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-300, high=-1, size=(3,v))
+    dummy_test(surf)
+    
+def test_14():
+    v = 40962
+    surf = {}
+    surf['coord'] = np.random.randint(low=-1000, high=-10, size=(3,v))
+    dummy_test(surf)
+    
+def test_15():
+    v = 40962
+    surf = {}
+    surf['coord'] = np.random.randint(low=-(v-1), high=(v+1), size=(3,v))
+    surf['coord'][0,:] = -1*abs(surf['coord'][0,:])     
+    dummy_test(surf)
+    
+def test_16():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.rand(3,v)
+    dummy_test(surf)
+
+def test_17():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-3, high=3, size=(3,v))
+    dummy_test(surf)
+    
+def test_18():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-300, high=300, size=(3,v))
+    dummy_test(surf)
+    
+def test_19():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.randint(low=-1000, high=1000, size=(3,v))
+    dummy_test(surf)
+    
+def test_20():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.randint(low=-(v-1), high=(v+1), size=(3,v))
+    dummy_test(surf)
+
+def test_21():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.rand(3,v)
+    surf['coord'][0,:] = np.ones((1,v)) * -1
+    dummy_test(surf)
+
+def test_22():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-3, high=3, size=(3,v))
+    surf['coord'][0,:] = np.ones((1,v)) * -1
+    dummy_test(surf)
+    
+def test_23():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-300, high=-1, size=(3,v))
+    dummy_test(surf)
+    
+def test_24():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.randint(low=-1000, high=-10, size=(3,v))
+    dummy_test(surf)
+    
+def test_25():
+    v = 163842
+    surf = {}
+    surf['coord'] = np.random.randint(low=-(v-1), high=(v+1), size=(3,v))
+    surf['coord'][0,:] = -1*abs(surf['coord'][0,:])     
+    dummy_test(surf)
+
+def test_26():
+    v = 327684
+    surf = {}
+    surf['coord'] = np.random.rand(3,v)
+    dummy_test(surf)
+
+def test_27():
+    v = 327684
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-3, high=3, size=(3,v))
+    dummy_test(surf)
+    
+def test_28():
+    v = 327684
+    surf = {}
+    surf['coord'] = np.random.uniform(low=-300, high=300, size=(3,v))
+    dummy_test(surf)
+    
+def test_29():
+    v = 327684
+    surf = {}
+    surf['coord'] = np.random.randint(low=-1000, high=1000, size=(3,v))
+    dummy_test(surf)
+    
+def test_30():
+    v = 327684
     surf = {}
     surf['coord'] = np.random.randint(low=-(v-1), high=(v+1), size=(3,v))
     dummy_test(surf)
